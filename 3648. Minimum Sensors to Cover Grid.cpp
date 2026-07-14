fb05a35dd6f2fae079296dfa176178be78c45c69
Vikashh08 <vikashroy2333@gmail.com>
2026-07-14 11:38:46 +0530

    Leetcode 3648

---
+++ 3648. Minimum Sensors to Cover Grid.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minSensors(int n, int m, int k) {
+        int side = 2 * k + 1;
+
+        int rows = (n + side - 1) / side;
+        int cols = (m + side - 1) / side;
+
+        return rows * cols;
+    }
+};
