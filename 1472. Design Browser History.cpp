99d4149d71f0690fa4617859188f76deb4710b72
Vikashh08 <vikashroy2333@gmail.com>
2026-07-24 12:29:05 +0530

    Leetcode 1472

---
+++ 1472. Design Browser History.cpp
@@ -0,0 +1,32 @@
+class BrowserHistory {
+    vector<string> history;
+    int curr, last;
+
+public:
+    BrowserHistory(string homepage) {
+        history.push_back(homepage);
+        curr = 0;
+        last = 0;
+    }
+
+    void visit(string url) {
+        curr++;
+
+        if (curr < history.size())
+            history[curr] = url;
+        else
+            history.push_back(url);
+
+        last = curr;
+    }
+
+    string back(int steps) {
+        curr = max(0, curr - steps);
+        return history[curr];
+    }
+
+    string forward(int steps) {
+        curr = min(last, curr + steps);
+        return history[curr];
+    }
+};
